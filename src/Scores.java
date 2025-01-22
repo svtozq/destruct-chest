aa11ddb9a24499771609f7d53208c275b90697a1
svtozq <rpenea@esiee-it>
2025-01-22 15:50:06 +0100

    ajout de la fonction pour mofifier le fichier scores

--- src/Scores.java
+++ src/Scores.java
@@ -1,40 +1,101 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scores {
-    private int lignes;
-    private int colonnes;
-    private String [][] tableau;
-
-    // fonction du tableau
-    public Scores(int lignes, int colonnes) {
-        this.lignes = lignes;
-        this.colonnes = colonnes;
-        this.tableau = new String[lignes][colonnes];
-
-        for (int i = 0; i < lignes; i++) {
-            for (int j = 0; j < colonnes; j++) {
-            }
-        }
-
-    }
-
-    // fonction d'affichage du tableau
-    public void afficheScore () {
-        // boucle qui affiche les lignes et colonnes du tableau
-        for (int i = 0; i < lignes; i++ ){
-            // boucle qui affiche les séparations
-            for (int j = 0; j < lignes; j++) {
-                System.out.println("----");
-            }
-
-            // boucle qui affiche les cases
-            for (int j = 0; j < colonnes; i++){
-                System.out.println("| " + tableau[i][j]);
-            }
-            System.out.println(" |");
-        }
-    }
-
     public static void main (String[] args) {
+        // chemin du fichier
+        String cheminFichier = "scores.txt";
 
+        String lignes;
+        String ligneAModifier = "Joueur 2";
+        String nouvelleLigne = "Joueur 2: 7";
+
+        try {
+            List<String> list = new ArrayList<>();
+            try (BufferedReader reader = new BufferedReader(new FileReader(cheminFichier))) {
+
+                // lecture du fichier ligne par ligne
+                while ((lignes = reader.readLine()) != null) {
+                    // remplace la ligne contenant le pseudo par le nouveau nombre de victoires
+                    if (lignes.contains(ligneAModifier)) {
+                        list.add(nouvelleLigne);
+                    } else {
+                        list.add(lignes);
+                    }
+                }
+
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(cheminFichier))) {
+                    for (String ligne : list) {
+                        writer.write(ligne);
+                        writer.newLine();
+                    }
+                }
+                System.out.println("Modification effectué avec succès !");
+                // en cas d'erreur
+            } } catch (IOException e) {
+            System.err.println("Erreur lors de la modification : " + e.getMessage());
+        }
+
+        verifierFichier(cheminFichier);
+
+        afficherScores(cheminFichier);
     }
 
-}
+    public static void verifierFichier (String cheminFichier) {
+        // fichier texte
+        File file = new File(cheminFichier);
+
+        // créer le fichier s'il n'existe pas
+        if (!file.exists()) {
+            try {
+                if (file.createNewFile()) {
+                    System.out.println("Création du fichier : " + cheminFichier);
+
+                    try (FileWriter writer = new FileWriter(file)) {
+                        writer.write("Joueur 1 : 0");
+                    }
+
+                }
+            } catch (IOException e) {
+                System.err.println("Erreur lors de la création du fichier : " + e.getMessage());
+            }
+        } else {
+            System.out.println("Le Fichier existe déjà : " + cheminFichier);
+            /*
+                    try (FileWriter writer = new FileWriter(file)) {
+                        writer.write("Joueur 1 : 0\nJoueur 1: 0\n");
+                    }
+                    */
+        }
+    }
+
+    public static void afficherScores (String cheminFichier) {
+        // utilisation d'une classe pour le contenu d'un fichier
+            try (BufferedReader reader = new BufferedReader(new FileReader(cheminFichier))) {
+                String ligne;
+                System.out.println("Tableau des Scores");
+
+                // lecture du fichier ligne par ligne
+                while ((ligne = reader.readLine()) != null) {
+                    // sépare les lignes en fonction du ':'
+                    String[] parts = ligne.split(":");
+                    // récupère et affiche les données des parties de la ligne
+                    if (parts.length == 2) {
+                        String pseudo = parts[0].trim();
+                        String victoires = parts[1].trim();
+                        System.out.println(pseudo + " a gagné " + victoires + " fois.");
+                    } else {
+                        System.out.println("Format invalide de la ligne : " + ligne);
+                    }
+                }
+                // en cas d'erreur
+            } catch (IOException e) {
+                System.err.println("Erreur lors de la lecture du fichier : " + e.getMessage());
+            }
+    }
+
+
+        }
+
+
